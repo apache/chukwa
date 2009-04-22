800f804a456b70bb03e6539824608cfe546b2c30
Eric Yang <eyang@apache.org>
2009-04-22 23:10:54 +0000

    CHUKWA-175. Removed error message for shutting down data processors. (Eric Yang)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/chukwa/branches/chukwa-0.1@767712 13f79535-47bb-0310-9956-ffa450edef68

--- bin/stop-data-processors.sh
+++ bin/stop-data-processors.sh
@@ -26,19 +26,19 @@ pidFile=$CHUKWA_PID_DIR/ProcessSinkFiles.pid
 if [ -f $pidFile ]; then  
    echo -n "Shutting down Data Processors.."
    DP_PID=`head ${pidFile}`
-   kill -TERM ${DP_PID}
+   kill -TERM ${DP_PID} > /dev/null 2>&1
    for i in 1 2 5; do
        test_pid=`ps ax | grep ${DP_PID} | grep -v grep | grep processSinkFiles.sh | wc -l`
        if [ $test_pid -ge 1 ]; then
            sleep $i
-           kill -TERM ${DP_PID}
+           kill -TERM ${DP_PID} > /dev/null 2>&1
        else
            break
        fi
    done
    test_pid=`ps ax | grep ${DP_PID} | grep -v grep | grep processSinkFiles.sh | wc -l`
    if [ $test_pid -ge 1 ]; then
-       kill -9 ${DBADMIN_PID} &>/dev/null
+       kill -9 ${DBADMIN_PID} > /dev/null 2>&1
    fi
    rm -f ${pidFile}
    rm -f $CHUKWA_PID_DIR/chukwa-$CHUKWA_IDENT_STRING-processSinkFiles.sh.pid
@@ -53,19 +53,19 @@ pidFile=$CHUKWA_PID_DIR/DemuxManager.pid
 if [ -f $pidFile ]; then
    echo -n "Shutting down demux.sh.."
    DEMUX_PID=`head ${pidFile}`
-   kill -TERM ${DEMUX_PID}
+   kill -TERM ${DEMUX_PID} > /dev/null 2>&1
    for i in 1 2 5; do
        test_pid=`ps ax | grep ${DEMUX_PID} | grep -v grep | grep DemuxManager | wc -l`
        if [ $test_pid -ge 1 ]; then
            sleep $i
-           kill -TERM ${DEMUX_PID}
+           kill -TERM ${DEMUX_PID} > /dev/null 2>&1
        else
            break
        fi
    done
    test_pid=`ps ax | grep ${DEMUX_PID} | grep -v grep | grep DemuxManager | wc -l`
    if [ $test_pid -ge 1 ]; then
-       kill -9 ${DEMUX_PID} &>/dev/null
+       kill -9 ${DEMUX_PID} > /dev/null 2>&1
    fi
    rm -f ${pidFile}
    rm -f $CHUKWA_PID_DIR/chukwa-$CHUKWA_IDENT_STRING-demux.sh.pid
@@ -80,19 +80,19 @@ pidFile=$CHUKWA_PID_DIR/PostProcessorManager.pid
 if [ -f $pidFile ]; then
    echo -n "Shutting down postProcess.sh ..."
    POST_PROCESS_PID=`head ${pidFile}`
-   kill -TERM ${POST_PROCESS_PID}
+   kill -TERM ${POST_PROCESS_PID} > /dev/null 2>&1
    for i in 1 2 5; do
        test_pid=`ps ax | grep ${POST_PROCESS_PID} | grep -v grep | grep PostProcessorManager | wc -l`
        if [ $test_pid -ge 1 ]; then
            sleep $i
-           kill -TERM ${POST_PROCESS_PID}
+           kill -TERM ${POST_PROCESS_PID} > /dev/null 2>&1
        else
            break
        fi
    done
    test_pid=`ps ax | grep ${POST_PROCESS_PID} | grep -v grep | grep PostProcessorManager | wc -l`
    if [ $test_pid -ge 1 ]; then
-       kill -9 ${POST_PROCESS_PID} &>/dev/null
+       kill -9 ${POST_PROCESS_PID} > /dev/null 2>&1 
    fi
    rm -f ${pidFile}
    rm -f $CHUKWA_PID_DIR/chukwa-$CHUKWA_IDENT_STRING-postProcess.sh.pid
@@ -106,19 +106,19 @@ pidFile=$CHUKWA_PID_DIR/ArchiveManager.pid
 if [ -f $pidFile ]; then
    echo -n "Shutting down archive.sh ..."
    POST_PROCESS_PID=`head ${pidFile}`
-   kill -TERM ${POST_PROCESS_PID}
+   kill -TERM ${POST_PROCESS_PID} > /dev/null 2>&1
    for i in 1 2 5; do
        test_pid=`ps ax | grep ${POST_PROCESS_PID} | grep -v grep | grep ChukwaArchiveManager | wc -l`
        if [ $test_pid -ge 1 ]; then
            sleep $i
-           kill -TERM ${POST_PROCESS_PID}
+           kill -TERM ${POST_PROCESS_PID} > /dev/null 2>&1
        else
            break
        fi
    done
    test_pid=`ps ax | grep ${POST_PROCESS_PID} | grep -v grep | grep ChukwaArchiveManager | wc -l`
    if [ $test_pid -ge 1 ]; then
-       kill -9 ${POST_PROCESS_PID} &>/dev/null
+       kill -9 ${POST_PROCESS_PID} > /dev/null 2>&1
    fi
    rm -f ${pidFile}
    rm -f $CHUKWA_PID_DIR/chukwa-$CHUKWA_IDENT_STRING-postProcess.sh.pid
@@ -132,19 +132,19 @@ pidFile=$CHUKWA_PID_DIR/dbAdmin.pid
 if [ -f $pidFile ]; then  
    echo -n "Shutting down Database Admin.."
    DBADMIN_PID=`head ${pidFile}`
-   kill -TERM ${DBADMIN_PID}
+   kill -TERM ${DBADMIN_PID} > /dev/null 2>&1
    for i in 1 2 5; do
        test_pid=`ps ax | grep ${DBADMIN_PID} | grep -v grep | grep dbAdmin.sh | wc -l`
        if [ $test_pid -ge 1 ]; then
            sleep $i
-           kill -TERM ${DBADMIN_PID}
+           kill -TERM ${DBADMIN_PID} > /dev/null 2>&1
        else
            break
        fi
    done
    test_pid=`ps ax | grep ${DBADMIN_PID} | grep -v grep | grep dbAdmin.sh | wc -l`
    if [ $test_pid -ge 1 ]; then
-       kill -9 ${DBADMIN_PID} &>/dev/null
+       kill -9 ${DBADMIN_PID} > /dev/null 2>&1
    fi
    rm -f ${pidFile}
    rm -f $CHUKWA_PID_DIR/chukwa-$CHUKWA_IDENT_STRING-dbAdmin.sh.pid
