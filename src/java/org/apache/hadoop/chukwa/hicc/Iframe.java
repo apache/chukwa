708a4d8f443b94bacb27a8ddff2763bb6dcd1408
Eric Yang <eyang@apache.org>
2009-08-31 16:48:58 +0000

    CHUKWA-386. Fix a bug in echo id to the browser for the iframe servlet. (Eric Yang)
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/chukwa/trunk@809657 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/hadoop/chukwa/hicc/Iframe.java
+++ src/java/org/apache/hadoop/chukwa/hicc/Iframe.java
@@ -13,17 +13,17 @@ import org.apache.hadoop.chukwa.util.XssFilter;
 
 public class Iframe extends HttpServlet {
   public static final long serialVersionUID = 100L;
-  private String id;
-  private String height = "100%";
-  private XssFilter xf = null;
 
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
+    String id;
+    String height = "100%";
+    XssFilter xf = null;
     xf = new XssFilter(request);
     if (xf.getParameter("boxId") != null) {
-      this.id = xf.getParameter("boxId");
+      id = xf.getParameter("boxId");
     } else {
-      this.id = "0";
+      id = "0";
     }
     response.setContentType("text/html; chartset=UTF-8//IGNORE");
     response.setHeader("boxId", xf.getParameter("boxId"));
@@ -48,9 +48,15 @@ public class Iframe extends HttpServlet {
         }
       }
     }
-    out.println("<html><body><iframe id=\"iframe" + this.id + "\" " + "src=\""
-        + source + "\" width=\"100%\" height=\"" + height + "\" "
-        + "frameborder=\"0\" style=\"overflow: hidden\"></iframe>");
+    StringBuffer output = new StringBuffer();
+    output.append("<html><body><iframe id=\"iframe");
+    output.append(id);
+    output.append("\" src=\"");
+    output.append(source);
+    output.append("\" width=\"100%\" height=\"");
+    output.append(height);
+    output.append("\" frameborder=\"0\" style=\"overflow: hidden\"></iframe>");
+    out.println(output.toString());
   }
 
   public void doPost(HttpServletRequest request, HttpServletResponse response)
